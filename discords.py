70b3c9cb90a24eb6ce4bbb71f1f3f86d3a40128e
oxy-Op <76278067+oxy-Op@users.noreply.github.com>
2021-05-16 22:04:10 +0530

    Add files via upload

---
+++ discords.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="o!",intents=discord.Intents.all(),self_bot = True)
+
+
+token = ""
+#YOUR TOKEN
+
+
+
+@bot.command()
+async def embed(ctx,title,description,footer,thumbnail,footericon):
+    embed = discord.Embed(title=str(title), description=str(description), color=discord.Color.blue())
+    embed.set_thumbnail(url=str(thumbnail))
+    embed.set_footer(text=str(footer),icon_url=str(footericon))
+    await ctx.send(embed=embed)
+
+
+bot.run(token,bot=False,reconnect=True)
